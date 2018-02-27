fad05952190f2a1a84b5d0b17b87a8997a93885d
Jepson <547212806@qq.com>
2018-02-28 00:11:07 +0800

    商品列表页完成

---
+++ src/components/numberBox/NumberBox.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="numberbox">
+    <mt-button class="minus" @click="minus">-</mt-button>
+    <input class="number" type="number" v-model="num">
+    <mt-button class="add" @click="add">+</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 1
+    }
+  },
+  methods: {
+    minus() {
+      if ( this.num <=0 ) return
+      this.num--;
+    },
+    add() {
+      this.num++;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.numberbox {
+  height: 30px;
+  line-height: 28px;
+  font-size: 0;
+  display: inline-block;
+  text-align: center;
+  vertical-align: bottom;
+  padding: 0 10px;
+}
+.minus {
+  width: 40px;
+  height: 100%;
+  vertical-align: bottom;
+}
+.add {
+  width: 40px;
+  height: 100%;
+  vertical-align: bottom;
+}
+.number {
+  width: 40px;
+  height: 100%;
+  border: 1px solid #ccc;
+  vertical-align: bottom;
+  font-size: 16px;
+  overflow: hidden;
+  padding: 0 3px;
+  text-align: center;
+}
+</style>
--- src/views/goods/GoodsDetail.vue
+++ src/views/goods/GoodsDetail.vue
@@ -1,23 +1,141 @@
 <template>
-  <div>
-    <p>商品详情页</p>
-    <p>商品详情页</p>
-    <p>商品详情页</p>
-    <p>商品详情页</p>
-    <p>商品详情页</p>
+  <div class="goodsdetail">
+    <!-- 轮播图 -->
+    <div class="slider">
+      <mt-swipe :auto="4000">
+        <mt-swipe-item>
+          <img src="http://vue.studyit.io/upload/201504/20/thumb_201504200046589514.jpg" alt="">
+        </mt-swipe-item>
+      </mt-swipe>
+    </div>
+
+    <!-- 商品介绍 -->
+    <div class="card">
+      <div class="card-head"> 华为（HUAWEI）荣耀6Plus 16G双4G版 </div>
+      <div class="card-item"> 
+        <div class="price">
+          <span>市场价: <i class="last">&yen;2499</i></span>
+          <span>销售: <i class="now">&yen;2195</i></span>
+        </div>
+        <div class="num">
+          购买数量: 
+          <numberbox></numberbox>
+        </div>
+        <div class="btns">
+          <mt-button type="primary" class="buy">立即购买</mt-button>
+          <mt-button type="danger" class="addcart">加入购物车</mt-button>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="card pro">
+      <div class="card-head"> 商品参数 </div>
+      <div class="card-item proinfo"> 
+        <p>商品货号：SD9102356032</p>
+        <p>库存情况：60件</p>
+        <p>上架时间：2015-04-20 00:51:03</p>
+      </div>
+      <div class="card-item probtns">
+        <mt-button type="primary" plain size="large">商品描述</mt-button>
+        <mt-button type="danger" plain size="large">商品评论</mt-button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import numberbox from "@/components/numberBox/NumberBox"
+
 export default {
   data() {
     return {
-
+      
     }
+  },
+  components: {
+    numberbox
   }
 }
 </script>
 
-<style>
+<style scoped lang="less">
+
+.goodsdetail {
+  padding: 10px;
+}
+.mint-swipe {
+  height: 250px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  img {
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+  }
+}
+
+.card {
+  margin: 10px 0;
+  min-height: 150px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding-bottom: 10px;
+
+  .card-head {
+    padding: 0 10px;
+    height: 44px;
+    line-height: 44px;;
+    border-bottom: 1px solid #ccc;
+    font-weight: bold;
+  }
+
+  .card-item {
+    padding: 10px;
+    border-bottom: 1px solid #ccc;
+    font-size: 14px;
+    line-height: 20px;
+
+    &:last-child {
+      border: none;
+    }
+
+    .price {
+      padding: 10px 0;
+      .last {
+        text-decoration: line-through;
+        margin-right: 5px;
+      }
+      .now {
+        font-style: normal;
+        color: red;
+        font-size: 16px;
+      }
+    }
+
+    .num {
+      line-height: 30px;
+    }
+
+    .btns {
+      margin-top: 10px;
+    }
+  }
+}
+
+.pro {
+  .proinfo{
+    font-size: 14px;
+    color: #919195;
+    line-height: 30px;
+  }
+
+  .probtns {
+    button {
+      margin-top: 10px;
+    }
+  }
+}
 
 </style>
